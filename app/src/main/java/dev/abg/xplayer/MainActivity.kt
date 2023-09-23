e81f4aace63a1edbb096f9ccc4f4e4532abd8297
XYXX02 <boldinov.vitaliy@gmail.com>
2023-09-23 08:50:33 +0300

    20230922

--- app/src/main/java/dev/abg/xplayer/MainActivity.kt
+++ app/src/main/java/dev/abg/xplayer/MainActivity.kt
@@ -2,10 +2,21 @@ package dev.abg.xplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val musicURI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+        val text = findViewById<TextView>(R.id.textView)
+        text.text = musicURI.toString()
+
+        val list :RecyclerView = findViewById(R.id.recyclerView)
+
     }
 }
